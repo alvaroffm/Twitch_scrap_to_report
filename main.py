57734a0c1128695a71c70b4f4f839d115b1a80e7
Buzz <a.ferreromoreno@altran.com>
2021-11-20 13:32:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import requests
+from twitchAPI.twitch import Twitch
+import time
+from datetime import datetime
+import os
+from IPython.display import clear_output
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import matplotlib as mpl
+from matplotlib import font_manager as fm,rcParams
+from PIL import Image
+from io import BytesIO
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('hola')
